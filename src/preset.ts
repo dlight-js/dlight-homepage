b9d6b9cc825844a60540a1d9bd19208facde5bb4
Duan Yihan <69476139+IanDxSSXX@users.noreply.github.com>
2023-02-25 00:14:13 +0800

    Initial commit

---
+++ src/preset.ts
@@ -0,0 +1,15 @@
+import Markit, {addBlockRule} from "@iandx/markit";
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css';
+
+
+export default function() {
+    addBlockRule({
+        name: "CodeBlock",
+        rule: "default",
+        view: (content, {language}) => {
+            const highlightedCode = hljs.highlight(content, {language}).value
+            return `<div style="background-color: ghostwhite; padding: 25px; border-radius: 5px"><pre><code>${highlightedCode}</code></pre></div>`
+        }
+    })
+}
